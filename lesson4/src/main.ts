7e1bc0004f0dd06db36fc201545551a870e20f60
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-07-09 13:50:59 +0200

    add lil gui for tweaking parameters

--- lesson4/src/main.ts
+++ lesson4/src/main.ts
@@ -1,38 +1,55 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import GUI from 'lil-gui';
+import gsap from 'gsap';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <canvas class="webgl"></canvas>
 `
 
+const params = {
+  doubleClickToggleFullscreen: false,
+  spin: () => {
+    gsap.to(group.rotation, { y: group.rotation.y + 2 * Math.PI })
+  },
+  shapeComplexity: 30,
+  wireframe: true,
+}
 const canvas = document.querySelector('.webgl')!;
 
-
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x181818)
 
 const group = new THREE.Group();
 scene.add(group);
 
-const colors = ["black", "blue", "white", "red", "black"];
+const colors = ["blue", "white", "red"];
+const meshes: THREE.MeshBasicMaterial[] = [];
 
+function createShapes() {
+  group.clear()
+  meshes.length = 0;
+  colors.forEach((color, i) => {
+    const count = params.shapeComplexity;
+    const positionsArray = new Float32Array(count * 3 * 3);
+    for (let i = 0; i < count * 3 * 3; i++) {
+      positionsArray[i] = (Math.random() - 0.5) * 2
+    }
 
-colors.forEach((color, i) => {
-  const count = 30;
-  const positionsArray = new Float32Array(count * 3 * 3);
-  for (let i = 0; i < count * 3 * 3; i++) {
-    positionsArray[i] = (Math.random() - 0.5) * 2
-  }
-
-  const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
-  const geometry = new THREE.BufferGeometry()
-  geometry.setAttribute("position", positionsAttribute)
-  const cube = new THREE.Mesh(
-    geometry,
-    new THREE.MeshBasicMaterial({ color, wireframe: true }),
-  )
-  cube.position.x = i * 2 - 2 * Math.floor(colors.length / 2);
-  group.add(cube)
-})
+    const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+    const geometry = new THREE.BufferGeometry()
+    geometry.setAttribute("position", positionsAttribute)
+    const material = new THREE.MeshBasicMaterial({ color, wireframe: params.wireframe });
+    meshes.push(material)
+    const cube = new THREE.Mesh(
+      geometry,
+      material,
+    )
+    cube.position.x = i * 2 - 2 * Math.floor(colors.length / 2);
+    group.add(cube)
+  })
+}
+createShapes()
 
 const sizes = {
   width: window.innerWidth,
@@ -45,7 +62,6 @@ camera.position.set(0, 1, 5)
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setClearColor(0x181818)
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -65,6 +81,7 @@ window.addEventListener('resize', () => {
 })
 
 window.addEventListener("dblclick", () => {
+  if (!params.doubleClickToggleFullscreen) return;
   if (!document.fullscreenElement) {
     canvas.requestFullscreen()
   } else {
@@ -87,3 +104,41 @@ const update = () => {
 }
 
 update()
+
+// #### DEBUG ####
+const gui = new GUI();
+gui.add(document, 'title')
+gui.add(params, 'doubleClickToggleFullscreen')
+gui
+  .add(group.position, 'y')
+  .min(-3)
+  .max(3)
+  .step(0.1)
+  .name('group elevation')
+
+gui
+  .add(group, 'visible')
+
+gui.add(params, 'wireframe')
+  .onChange(createShapes)
+
+gui.add(params, 'spin')
+gui.add(params, 'shapeComplexity')
+  .min(3)
+  .max(300)
+  .step(1)
+  .onChange(createShapes)
+
+
+gui.addColor(scene, 'background')
+  .name("background color")
+
+gui.add(camera, 'fov')
+  .min(10)
+  .max(180)
+  .onChange(() => camera.updateProjectionMatrix())
+const colorsGUI = gui.addFolder('Colors')
+meshes.forEach((m, i) =>
+  colorsGUI.addColor(m, 'color')
+    .name(`Cube ${i} color`)
+)
