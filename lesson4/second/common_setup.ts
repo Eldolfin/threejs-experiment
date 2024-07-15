859e4e3a63422b677f583af790b6c655a3da4fce
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-07-15 15:37:24 +0200

    started lighting second scene

---
+++ lesson4/second/common_setup.ts
@@ -0,0 +1,62 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+
+export const setup3D = (
+  { onUpdate }: { onUpdate?: () => void },
+) => {
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<canvas class="webgl"></canvas>
+`;
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(sizes.width, sizes.height);
+  });
+
+  const scene = new THREE.Scene();
+
+  const canvas = document.querySelector<HTMLCanvasElement>('.webgl')!;
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+  });
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+  camera.position.set(0, 1, 5);
+  scene.add(camera);
+
+  const orbit = new OrbitControls(camera, renderer.domElement);
+  orbit.enableDamping = true;
+  orbit.update();
+
+  const update = () => {
+    window.requestAnimationFrame(update);
+
+    if (onUpdate) {
+      onUpdate();
+    }
+
+    orbit.update();
+    renderer.render(scene, camera);
+  };
+
+  update();
+
+  return {
+    scene,
+    camera,
+    renderer,
+    canvas,
+    orbit,
+  };
+};
--- lesson4/second/main.ts
+++ lesson4/second/main.ts
@@ -1,4 +1,66 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<canvas class="webgl"></canvas>
-🚧 WIP 🚧
-`;
+import * as THREE from 'three';
+import { setup3D } from './common_setup';
+import { TransformControls } from 'three/examples/jsm/Addons.js';
+import { GUI } from 'lil-gui';
+
+const { scene, camera, canvas, orbit } = setup3D({});
+
+const material = new THREE.MeshStandardMaterial();
+
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(),
+  material,
+);
+
+floor.position.set(0, -1, 0);
+floor.rotation.x = -Math.PI / 2;
+floor.scale.set(10, 10, 1);
+
+scene.add(floor);
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5),
+  material,
+);
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(),
+  material,
+);
+cube.position.set(1, 0, 0);
+sphere.position.set(-1, 0, 0);
+scene.add(sphere, cube);
+
+// scene.add(
+//   new THREE.AmbientLight(0xffffff, 0.5),
+// );
+
+// const directionalLight = new THREE.DirectionalLight(0x00ffff, 0.5);
+// directionalLight.position.set(1, 0.25, 0);
+// scene.add(directionalLight);
+
+// const hemisphereLight = new THREE.HemisphereLight(0xff00000, 0x0000ff, 3);
+// const hemisphereLightHelper = new THREE.HemisphereLightHelper(
+//   hemisphereLight,
+//   0.3,
+// );
+// scene.add(hemisphereLight, hemisphereLightHelper);
+
+const pointLight = new THREE.PointLight(0xffffff, 3, 10);
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.1);
+pointLight.position.set(0, 1, 0);
+const transformControls = new TransformControls(camera, canvas);
+transformControls.attach(pointLight);
+transformControls.addEventListener('dragging-changed', (event) => {
+  orbit.enabled = !event.value;
+});
+
+scene.add(pointLight, pointLightHelper, transformControls);
+
+/**
+ * DEBUG GUI
+ */
+const gui = new GUI();
+gui.add(pointLight, 'intensity', 0, 10);
+gui.add(pointLight, 'distance', 0, 10);
+gui.add(pointLight, 'decay', 1, 10);
+gui.addColor(pointLight, 'color');
